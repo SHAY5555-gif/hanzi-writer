877152930a3203e3e5586973cfb468ccf9aabb3e
SHAY5555-gif <134857867+SHAY5555-gif@users.noreply.github.com>
2025-07-03 16:46:41 +0300

    feat: Add Pinyin support and enhance character button display in demo

---
+++ demo/pinyinPro.js
@@ -0,0 +1,3 @@
+// Expose pinyin-pro (installed via npm) as global `window.pinyinPro` for plain-script demos
+import * as pinyinPro from '../node_modules/pinyin-pro/dist/index.js';
+window.pinyinPro = pinyinPro;
